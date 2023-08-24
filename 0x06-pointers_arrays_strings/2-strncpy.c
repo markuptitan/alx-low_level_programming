4588fd93d89047fe15d0a1ae6585de0c4ffc86d5
bytedojo <samsonlukhele76@gmail.com>
2023-08-24 09:38:59 +0200

    Rename 2-strncopy.c to 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies up to n bytes of one string to another
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ * @n: Maximum number of bytes to copy
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
