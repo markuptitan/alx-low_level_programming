d1c9d83814f07eb96245fdb69fb2d585aa6ad6c1
bytedojo <samsonlukhele76@gmail.com>
2023-09-02 17:15:59 +0200

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,11 @@
 
 void print_alphabet_x10(void)
 {
+int i,j;
 char letter = 'a';
-for (int i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-for (int j = 0; j < 26; j++)
+for (j = 0; j < 26; j++)
 {
 putchar(letter);
 letter++;
