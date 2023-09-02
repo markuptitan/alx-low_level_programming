01ecf57f4b662e1c4daeb0771646a27f81b91213
bytedojo <samsonlukhele76@gmail.com>
2023-09-02 17:07:20 +0200

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/**
+ * print_alphabet_x10 - Entry point for function
+ *
+ *
+*/
+
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+char letter = 'a';
+for (int i = 0; i < 10; i++)
+{
+for (int j = 0; j < 26; j++)
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+letter = 'a';
+}
+}
