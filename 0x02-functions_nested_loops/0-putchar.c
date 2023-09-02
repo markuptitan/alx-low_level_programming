cdbb3d9d4734d1a821a0fc7f0c580180ed12792e
bytedojo <samsonlukhele76@gmail.com>
2023-09-02 16:18:19 +0200

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+*main - Entry point for execution
+*Return: (0) Always Success
+*
+*/
+#include <stdio.h>
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('\n');
+  return(0);
+}
